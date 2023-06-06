69069edb49c0bf3eb281f6a97ed8cda721ea50f7
tenzinMetok <tenzin.m@collpoll.com>
2023-06-06 14:41:44 +0530

    filters

--- app/app.js
+++ app/app.js
@@ -2,9 +2,27 @@
 var myApp=angular.module('myApp',[]);
 // creating a controller
 myApp.controller("myCtrl",function($scope){
-    $scope.message="welcome here";
-    $scope.handleClick=function(){
-        alert("you clicked me!!");
-    }
-    $scope.arr=['aaa','bbb','ccc','ddd'];
+    $scope.arr=[
+        {
+            name:"dasdsd",
+            age:22,
+            fees:2324
+        },
+        {
+            name:"fdgdfs",
+            age:23,
+            fees:4532
+        },
+        {
+            name:"nbcvcv",
+            age:52,
+            fees:5435
+        },
+        {
+            name:"adsfad",
+            age:25,
+            fees:7656
+        }
+        
+    ];
 })
