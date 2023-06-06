50dcb2d2f3603c7902411caad563caa4bcfd2cb8
tenzinMetok <tenzin.m@collpoll.com>
2023-06-06 16:59:58 +0530

    ng-submit

--- app/app.js
+++ app/app.js
@@ -8,6 +8,19 @@ myApp.controller("myCtrl",function($scope){
         var removedArr=$scope.arr.indexOf(i);
         $scope.arr.splice(removedArr,1);
     }
+    $scope.addArr=function(){
+        console.log("===========");
+        $scope.arr.push({
+            name:$scope.newArr.name,
+            age: parseInt($scope.newArr.age),
+            fees: parseInt($scope.newArr.fees),
+            available: true
+        });
+    $scope.newArr.name="";
+    $scope.newArr.age="";
+    $scope.newArr.fees="";
+
+    }
     $scope.arr=[
         {
             name:"dasdsd",
